3713db8406dd857134e8f3aaff1b37bcc0a571d3
zhuchcn <ChenghaoZhu@mednet.ucla.edu>
2022-11-14 07:54:45 -0800

    add action to build and deploy pipeline documentation site

---
+++ render-and-deploy-docs/action.sh
@@ -0,0 +1,28 @@
+set -euo pipefail
+
+set_git() {
+    remote_repo="https://x-access-token:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
+    git remote rm origin
+    git remote add origin "${remote_repo}"
+
+    if ! git config --get user.name; then
+        git config --global user.name "${GITHUB_ACTOR}"
+    fi
+
+    if ! git config --get user.email; then
+        git config --global user.email "${GITHUB_ACTOR}@users.noreply.github.com"
+    fi
+}
+
+build_and_deploy() {
+    config_file=${GITHUB_WORKSPACE}/mkdocs.yml
+    mkdocs gh-deploy --config-file $CONFIG_FILE --force
+}
+
+main() {
+    set_git
+    python build_config_yaml_from_readme.py
+    build_and_deploy
+}
+
+main
---
+++ render-and-deploy-docs/build_config_yaml_from_readme.py
@@ -0,0 +1,83 @@
+""" """
+from typing import Dict, List
+import os
+from pathlib import Path
+import re
+import yaml
+
+
+def get_workspace() -> Path:
+    """ """
+    return Path(os.getenv('GITHUB_WORKSPACE'))
+
+def split_readme() -> Dict[str, Path]:
+    """ """
+    contents:Dict[str, List[str]] = {}
+    paths:Dict[str, Path] = {}
+    work_dir = get_workspace()
+    readme_file = work_dir/'README.md'
+    docs_dir = work_dir/'docs'
+    cur = None
+    with open(readme_file, 'rt') as handle:
+        line:str
+        for line in handle:
+            if line.startswith('## '):
+                header = re.sub('^#+ ', '', line.rstrip())
+                if header.lower() == 'overview':
+                    header = 'Home'
+                cur = contents.get(header)
+                if cur is None:
+                    cur = []
+                    doc_file_name = header.lower().replace(' ','-') + '.md'
+                    doc_file_path = docs_dir/doc_file_name
+                    paths[header] = doc_file_path
+
+            if cur is None:
+                continue
+
+            cur.append(line)
+
+    docs_dir.mkdir(exist_ok=True)
+
+    for key, content in contents.items():
+        path = paths[key]
+        with open(path, 'w') as handle:
+            for line in content:
+                handle.write(line)
+
+    return paths
+
+def get_pipeline_name():
+    """ """
+    repo_name = os.getenv('GITHUB_REPOSITORY').split('/')[1]
+    pipeline_name = repo_name
+    for key in ['pipeline', 'metapipeline']:
+        if pipeline_name.startswith(key):
+            pipeline_name.replace(key, '')
+            break
+    return pipeline_name
+
+def load_metadata():
+    """ """
+    work_dir = get_workspace()
+    with open(work_dir, 'r') as handle:
+        return yaml.safe_load(handle)
+
+def build_mkdocs_config():
+    """ """
+    paths = split_readme()
+    config_data = {
+        'site_name': get_pipeline_name(),
+        'docs_dir': 'docs/',
+        'repo_url': 'https://github.com/' + os.getenv('GITHUB_REPOSITORY'),
+        'nav': {key:val.name for key,val in paths.items()},
+        'theme': 'readthedocs',
+        'markdown_extensions': ['tables', 'admonition']
+    }
+    workspace = get_workspace()
+    with open(workspace/'mkdocs.yml', 'w') as handle:
+        yaml.dump(config_data, handle)
+
+
+if __name__ == '__main__':
+    build_mkdocs_config()
