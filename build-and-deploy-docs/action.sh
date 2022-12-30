afbfbd94fa9cedfc6c47e52ea29ff3b74af288ba
zhuchcn <ChenghaoZhu@mednet.ucla.edu>
2022-12-30 01:08:36 -0800

    set ownership all GITHUB_WORKSPACE

--- build-and-deploy-docs/action.sh
+++ build-and-deploy-docs/action.sh
@@ -10,9 +10,6 @@ set_git() {
     git remote rm origin
     git remote add origin "${remote_repo}"
 
-    # see https://github.com/actions/checkout/issues/766
-    git config --global --add safe.directory "${GITHUB_WORKSPACE}"
-
     if ! git config --get user.name; then
         git config --global user.name "${GITHUB_ACTOR}"
     fi
@@ -22,18 +19,25 @@ set_git() {
     fi
 }
 
+set_ownership() {
+    chown -R $(id -u):$(id -u) ${GITHUB_WORKSPACE}
+    # see https://github.com/actions/checkout/issues/766
+    git config --global --add safe.directory "${GITHUB_WORKSPACE}"
+}
+
 build_and_deploy() {
     config_file=${GITHUB_WORKSPACE}/mkdocs.yml
     mkdocs gh-deploy --config-file $config_file --force
 }
 
 main() {
+    set_git
     python /src/create_mkdocs_config.py \
         --pipeline-dir ${GITHUB_WORKSPACE} \
         --pipeline-repo ${GITHUB_REPOSITORY} \
         --mkdocs-config ${MKDOCS_CONFIG} \
         --readme ${README}
-    set_git
+    set_ownership
     build_and_deploy
 }
 
