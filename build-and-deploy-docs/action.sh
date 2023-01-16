a634b2adffe5abfaf06152e05786a28a74069b4a
zhuchcn <ChenghaoZhu@mednet.ucla.edu>
2023-01-16 00:50:07 -0800

    always set user name

--- build-and-deploy-docs/action.sh
+++ build-and-deploy-docs/action.sh
@@ -8,18 +8,13 @@ TOKEN=$3
 set_git() {
     # see https://github.com/actions/checkout/issues/766
     git config --global --add safe.directory "${GITHUB_WORKSPACE}"
-    
+
     remote_repo="https://x-access-token:${TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
     git remote rm origin
     git remote add origin "${remote_repo}"
 
-    if ! git config --get user.name; then
-        git config --global user.name "${GITHUB_ACTOR}"
-    fi
-
-    if ! git config --get user.email; then
-        git config --global user.email "${GITHUB_ACTOR}@users.noreply.github.com"
-    fi
+    git config --global user.name "${GITHUB_ACTOR}"
+    git config --global user.email "${GITHUB_ACTOR}@users.noreply.github.com"
 }
 
 build_and_deploy() {
