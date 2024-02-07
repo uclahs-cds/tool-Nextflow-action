a51bdc91eab738d0b4064fe95de9ac74a3369f80
Nicholas Wiltsie <nwiltsie@mednet.ucla.edu>
2024-02-07 09:35:19 -0800

    Comment code, cleanup

--- build-and-deploy-docs/create_mkdocs_config.py
+++ build-and-deploy-docs/create_mkdocs_config.py
@@ -20,7 +20,6 @@ from markdown_it.token import Token
 from mdformat.renderer import RenderContext, RenderTreeNode
 
 
-
 VALID_IMAGE_MIME_TYPES = {
     'image/png',
     'image/jpeg',
@@ -31,7 +30,6 @@ VALID_IMAGE_MIME_TYPES = {
     'image/svg+xml'
 }
 
-# pylint: disable=R0914
 
 def parse_args():
     """ parse args """
@@ -40,15 +38,15 @@ def parse_args():
         '--pipeline-dir',
         type=Path,
         required=True,
-        help='Path to the pipeline directory. Should be set to GITHUB_WORKSPACE'
-        ' when called from github action.'
+        help='Path to the pipeline directory. Should be set to '
+        'GITHUB_WORKSPACE when called from github action.'
     )
     parser.add_argument(
         '--pipeline-repo',
         type=str,
         required=True,
-        help='Pipeline repo name. Should be set to GITHUB_REPOSITORY when called'
-        ' from github action.'
+        help='Pipeline repo name. Should be set to GITHUB_REPOSITORY '
+        ' when called from github action.'
     )
     parser.add_argument(
         '--mkdocs-config',
@@ -63,28 +61,6 @@ def parse_args():
         default=Path('README.md')
     )
     return parser.parse_args()
-
-
-def is_url(path: str) -> bool:
-    """ Checks if the given path is an url. """
-    return path.startswith('http')
-
-
-def validate_image(path: str, work_dir: Path) -> Path:
-    """ Check whether the given path is a valid image file and return the
-    cleaned and resolved path. """
-    path = path.split('?')[0]
-    validated_path = Path(path)
-
-    if not validated_path.is_absolute():
-        validated_path = work_dir / validated_path
-
-    if not str(validated_path.resolve()).startswith(str(work_dir.resolve())) \
-            or not validated_path.exists() \
-            or magic.from_file(str(validated_path), mime=True) not in VALID_IMAGE_MIME_TYPES:
-        raise ValueError(f'The given path {validated_path} is invalid.')
-
-    return validated_path
 
 
 def get_heading_anchor(text):
@@ -113,29 +89,38 @@ class Page:
         return self.filename
 
 
-def split_readme_new(readme_file: Path,
-                     docs_dir: Path,
-                     pipeline_repo: str) -> Dict[str, Path]:
+def split_readme(readme_file: Path,
+                 docs_dir: Path,
+                 pipeline_repo: str) -> List[Dict[str, Path]]:
     """
     Split the README file into individual markdown files.
+
+    Return a list of {<title>: <filename>} dictionaries suitable for a MkDocs
+    nav element.
     """
+    # pylint: disable=too-many-locals
     img_dir = docs_dir / 'img'
 
     docs_dir.mkdir(exist_ok=True)
     img_dir.mkdir(exist_ok=True)
 
-    # Parse the original markdown file into tokens
+    # Parse the original markdown file into a stream of tokens
     with readme_file.open(encoding="utf-8") as infile:
         tokens = MarkdownIt("gfm-like").parse(infile.read())
 
     # Break the monolithic page into multiple pages on H2s. Name the pages by
-    # the content of their headings. Simultaneously, build up a corrected set
-    # of anchor links
+    # the content of their headings.
+    pages = [Page("Home", filename="index.md"), ]
+
+    # Simultaneously build up a corrected set of anchor links. As we're
+    # splitting one file into multiple, an anchor link like `#authors` will
+    # need to include the filename, like `repo-details.md#authors`.
     anchor_pages = {}
 
-    current_page = Page("Home", filename="index.md")
-    pages = [current_page, ]
-
+    # MarkdownIt treats headers as three sequential tokens: heading_open,
+    # inline (which will have child tokens), and heading_close. Iterate through
+    # with itertools.pairwise so that we'll have access to heading_open and
+    # inline simultaneously.
     for token, next_token in itertools.pairwise(
             itertools.chain(tokens, [None, ])):
         if token.type == "heading_open":
@@ -143,87 +128,80 @@ def split_readme_new(readme_file: Path,
 
             if token.tag == "h2":
                 # We've moved on to a new page
-                current_page = Page(heading_content)
-                pages.append(current_page)
+                pages.append(Page(heading_content))
 
-            # Associate this heading with the the current page
+            # Associate this anchor with the the current page
             anchor = get_heading_anchor(heading_content)
             assert anchor not in anchor_pages
-            anchor_pages[anchor] = current_page.get_filename()
+            anchor_pages[anchor] = pages[-1].get_filename()
 
-        current_page.tokens.append(token)
+        pages[-1].tokens.append(token)
 
     def sanitize_link(url):
+        """
+        Sanitize a link within the README.
+
+        There are five cases to be handled:
+        1. Links outside of the repository - leave unchanged
+        2. File links within the docs/ folder - rewrite
+        3. Images - copy into docs/ and rewrite
+        4. File links within the repository - redirect to the code browser
+        5. Anchor links - rewrite to reference the newly split pages
+        """
         link = urlparse(url)
 
         if link.scheme or link.netloc:
             # This is a "real" link (https://, ftp://, etc.) - don't touch it
-            return url
+            pass
 
-        if link.path:
+        elif link.path:
             # This is a link to a file on disk
             resolved_path = Path(readme_file.parent, link.path).resolve()
 
-            # If the path reaches outside of the repo, bail out
-            if not resolved_path.is_relative_to(readme_file.parent):
-                return url
+            # Only mess with paths within the repository
+            if resolved_path.is_relative_to(readme_file.parent):
+                filetype = magic.from_file(resolved_path, mime=True)
 
-            # If the path is already under the docs directory, correct it
-            if resolved_path.is_relative_to(docs_dir):
-                return urlunparse((
-                    "",     # scheme
-                    "",     # netloc
-                    str(resolved_path.relative_to(docs_dir)),
-                    link.params,
-                    link.query,
-                    link.fragment
-                ))
+                # If the path is already under the docs/ directory, correct it
+                # (the linking document will now be under docs/ as well)
+                if resolved_path.is_relative_to(docs_dir):
+                    link = link._replace(
+                        path=str(resolved_path.relative_to(docs_dir))
+                    )
 
-            # If the link is to an image, copy that image to the docs
-            filetype = magic.from_file(resolved_path, mime=True)
-            if filetype in VALID_IMAGE_MIME_TYPES:
-                output_path = Path(img_dir, resolved_path.name)
-                shutil.copy2(resolved_path, output_path)
+                # If the link is to an image, copy that image to the docs
+                elif filetype in VALID_IMAGE_MIME_TYPES:
+                    output_path = Path(img_dir, resolved_path.name)
+                    shutil.copy2(resolved_path, output_path)
 
-                return urlunparse((
-                    "",     # scheme
-                    "",     # netloc
-                    str(output_path.relative_to(docs_dir)),
-                    link.params,
-                    link.query,
-                    link.fragment
-                ))
+                    link = link._replace(
+                        path=str(output_path.relative_to(docs_dir))
+                    )
 
-            # For everything else, link to the file on GitHub
-            return urlunparse((
-                "https",
-                "github.com",
-                str(Path(
-                    pipeline_repo,
-                    "blob",
-                    "main",
-                    resolved_path.relative_to(readme_file.parent)
-                )),
-                link.params,
-                link.query,
-                link.fragment
-            ))
+                else:
+                    # For everything else, link to the file on GitHub
+                    link = link._replace(
+                        scheme="https",
+                        netloc="github.com",
+                        path=str(Path(
+                            pipeline_repo,
+                            "blob",
+                            "main",
+                            resolved_path.relative_to(readme_file.parent)
+                        ))
+                    )
 
-        if link.fragment:
+        elif link.fragment:
             # This is an anchor link. As we've split the monolithic README into
             # multiple files, we need to prepend those filepaths.
-            return urlunparse((
-                "",     # scheme
-                "",     # netloc
-                anchor_pages[get_heading_anchor(link.fragment)],
-                "",     # params
-                "",     # query
-                link.fragment
-            ))
+            link = link._replace(
+                path=anchor_pages[get_heading_anchor(link.fragment)],
+            )
 
-        return url
+        return urlunparse(link)
 
-    # Examine and correct all links (if necessary)
+    # Iterate through all tokens (including child tokens) to rewrite links and
+    # copy images into the docs/ folder
     tokens_to_examine = collections.deque(tokens)
     while tokens_to_examine:
         token = tokens_to_examine.popleft()
@@ -248,8 +226,7 @@ def split_readme_new(readme_file: Path,
     table_of_contents = []
 
     for page in pages:
-        fullpath = Path(docs_dir, page.get_filename())
-        fullpath.write_text(
+        Path(docs_dir, page.get_filename()).write_text(
             renderer.render(page.tokens, options, {}),
             encoding="utf-8"
         )
@@ -264,7 +241,8 @@ def get_pipeline_name(repo: str):
     pipeline_name = repo.rsplit('/', maxsplit=1)[-1]
     return pipeline_name
 
-def get_mkdocs_config_data(path:Path, repo:str):
+
+def get_mkdocs_config_data(path: Path, repo: str):
     """ Read the given MKDocs config file or create it from default.
 
     Args:
@@ -272,23 +250,29 @@ def get_mkdocs_config_data(path:Path, repo:str):
           config is used.
         - `repo`: The github repo name.
     """
-    if path is not None:
-        with open(path, 'rt') as handle:
-            data = yaml.safe_load(handle)
-            if 'nav' not in data:
-                data['nav'] = []
-            return data
-    else:
-        return {
-            'site_name': get_pipeline_name(repo),
-            'docs_dir': 'docs/',
-            'repo_url': 'https://github.com/' + repo,
-            'nav': [],
-            'theme': 'readthedocs',
-            'markdown_extensions': ['tables', 'admonition'],
-            'edit_uri_template': 'blob/main/README.md',
-            'plugins': ['mike'],
-        }
+    config = {
+        'site_name': get_pipeline_name(repo),
+        'docs_dir': 'docs/',
+        'repo_url': 'https://github.com/' + repo,
+        'theme': 'readthedocs',
+        'edit_uri_template': 'blob/main/README.md',
+        'nav': [],
+    }
+
+    if path:
+        # Update the defaults with the local configuration
+        config.update(yaml.safe_load(path.read_bytes()))
+
+    # Add in the plugins and extensions that we require
+    required_plugins = {"mike"}
+    plugins = config.setdefault("plugins", [])
+    plugins.extend(sorted(required_plugins - set(plugins)))
+
+    required_extensions = {"tables", "admonition"}
+    extensions = config.setdefault("markdown_extensions", [])
+    extensions.extend(sorted(required_extensions - set(extensions)))
+
+    return config
 
 
 def build_mkdocs_config():
@@ -308,7 +292,7 @@ def build_mkdocs_config():
     config_data = get_mkdocs_config_data(mkdocs_config, repo)
 
     if args.readme:
-        readme_nav = split_readme_new(
+        readme_nav = split_readme(
             readme_file=work_dir/args.readme,
             docs_dir=work_dir/config_data['docs_dir'],
             pipeline_repo=args.pipeline_repo
@@ -316,8 +300,12 @@ def build_mkdocs_config():
 
         config_data['nav'] = readme_nav + config_data['nav']
 
-    with open(work_dir/'mkdocs.yml', 'w', encoding="utf-8") as handle:
-        yaml.dump(config_data, handle, explicit_start=True)
+    with Path(work_dir, "mkdocs.yml").open("w", encoding="utf-8") as outfile:
+        yaml.safe_dump(
+            config_data,
+            outfile,
+            explicit_start=True,
+            sort_keys=False)
 
 
 if __name__ == '__main__':
